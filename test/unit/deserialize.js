756393e19c6ef5d4e559f6769c9d36cdfa34940d
Pine Mizune <pinemz@gmail.com>
2015-02-04 14:53:31 +0900

    Remove unused lines

--- test/unit/deserialize.js
+++ test/unit/deserialize.js
@@ -1,10 +1,6 @@
-var _ = require('lodash');
 var chai = require('chai');
-var sinon = require('sinon');
-var sinonChai = require('sinon-chai');
 
 var expect = chai.expect;
-chai.use(sinonChai);
 
 var events = require('events');
 var serialize = require('../../lib/serialize');
